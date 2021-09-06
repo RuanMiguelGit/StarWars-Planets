4f21c6354ecf89ae6b2033d97c73415cf6c63266
Ruan Miguel <ruanmiguelps@gmail.com>
2021-09-06 14:22:40 -0300

    go

---
+++ build/precache-manifest.89be004190bdb8b2eca695d530088543.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a959668942d6d3d637904e8d6ab3b21e",
+    "url": "/StarWars-Planets/index.html"
+  },
+  {
+    "revision": "27b321fd0ecf6d8771b9",
+    "url": "/StarWars-Planets/static/css/main.c8e09488.chunk.css"
+  },
+  {
+    "revision": "8ed342863d59b1fbb06c",
+    "url": "/StarWars-Planets/static/js/2.df4b98a3.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/StarWars-Planets/static/js/2.df4b98a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27b321fd0ecf6d8771b9",
+    "url": "/StarWars-Planets/static/js/main.dfdd7cc2.chunk.js"
+  },
+  {
+    "revision": "247becb4d21b0e4c4b78",
+    "url": "/StarWars-Planets/static/js/runtime-main.1310a904.js"
+  }
+]);
